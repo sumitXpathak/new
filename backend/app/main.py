f6a05549a1268fc398960d00e28bed04ba4ee42f
SumitXPathak <sumitpathak219@gmail.com>
2025-11-10 22:31:12 +0530

    ok

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://your-portfolio-domain.com"  # Add your production domain
+        "https://https://new-sigma-orpin.vercel.app/"  # Add your production domain
     ],
     allow_credentials=True,
     allow_methods=["*"],
