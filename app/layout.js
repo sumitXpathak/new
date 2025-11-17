6ddaa0c09d8651c1eaa9bc013b9a164b789e5a8c
SumitXPathak <sumitpathak219@gmail.com>
2025-11-17 20:46:38 +0530

    c

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -20,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
